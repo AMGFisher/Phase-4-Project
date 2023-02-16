1f75d65946447ea65d0cd79b9d2d73c6991ba792
James Cogan <james.m.cogan@gmail.com>
2023-02-16 17:53:21 -0500

    new css

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -20,7 +20,7 @@ export function Profile({ user }) {
 
     return ( <>
     <h1>Profile</h1>
-    <h2>Handle: @{user.handle}</h2>
+    <h2>Handle: @{user.handle} className=</h2>
     <h3>Name: {user.first_name} {user.last_name}</h3>
     <h4>Contact: {user.email}</h4>
     <div>{profilePostCards}</div>
