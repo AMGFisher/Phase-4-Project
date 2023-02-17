61bf141742303b07d3babf97cbfb25f7602236f4
Adam Fisher <amgfisher90@gmail.com>
2023-02-17 11:06:55 -0500

    Feed React Display

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,53 +1,28 @@
-import FeedCard from './FeedCard';
 import { useEffect, useState } from 'react';
 
-export function Feed({user}) {
+import FeedCard from './PostCard';
+
+export function Feed() {
 
     const [posts, setPosts] = useState([]);
 
-
     useEffect(() => {
-        fetch("/follows")
+        fetch("/feed")
           .then((r) => r.json())
           .then(setPosts);
       }, []);
 
-    const post = posts.map((post) => {
-        console.log(post.posts)
+      console.log(posts)
 
-    })
+      const feedCards = posts.map((post) => (
+        <FeedCard
+        key={post.id}
+        post={post}
+        />
+      ))
 
-
-
-    return <h1>Personal Feed</h1>
-
-
+    return <>
+    <h1 className="homePageTitle">Personal Feed</h1>
+    <div className='postCard'>{feedCards}</div>
+    </>
 }
-
-
-
-
-// export function Home() {
-
-//     const [posts, setPosts] = useState([]);
-
-//     useEffect(() => {
-//         fetch("/posts")
-//           .then((r) => r.json())
-//           .then(setPosts);
-//       }, []);
-
-//       console.log(posts)
-
-//       const postCards = posts.map((post) => (
-//         <PostCard
-//         key={post.id}
-//         post={post}
-//         />
-//       ))
-
-//     return <>
-//     <h1>Home</h1>
-//     <div>{postCards}</div>
-//     </>
-// }
